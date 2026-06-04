4f1077d64a5f0d21e9a5b59da9dc99193d3aa6a6
Vijana Maparathna <kaushalyaofficial@gmail.com>
2026-06-05 00:04:32 +0530

    update db config for render

--- src/config/db.js
+++ src/config/db.js
@@ -5,12 +5,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false
 });
+
 pool.on('connect', () => {
     console.log('Connected to the database');
 });
